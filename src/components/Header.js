73647d3e6d9584c70ea37373fd985142468c1c90
William Crownover <williamcrownover1@gmail.com>
2021-11-21 20:11:13 -0800

    Added nav link objects and styled nav component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,39 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
 
+	const navData = [
+		{
+			id: 1,
+			to: '/',
+			name: 'Home'
+		},
+		{
+			id: 2,
+			to: '/about',
+			name: 'About Dr. Bailey'
+		},
+		{
+			id: 3,
+			to: '/clinical-services',
+			name: 'Clinical Services'
+		},
+		{
+			id: 4,
+			to: '/covid',
+			name: 'COVID-19'
+		},
+		{
+			id: 5,
+			to: '/contact',
+			name: 'Contact'
+		},
+		{
+			id: 6,
+			to: '/consultation',
+			name: 'Trans Inclusive Clinic Consultation'
+		}
+	]
+
 	const [navOpen, setNavOpen] = useState(false);
 
 	const openNav = () => {
@@ -19,11 +52,15 @@ export default function Header() {
 
 			<div className={`sidenav ${navOpen ? "open" : "closed"}`}>
 				<div href="#" className="closebtn" onClick={closeNav}>&times;</div>
-				<Link
-					to="/about"
-				>
-					About Dr. Bailey
-				</Link>
+				{navData.map((page) => (
+					<Link
+						key={page.id}
+						to={page.to}
+						onClick={closeNav}
+					>
+						{page.name}
+					</Link>
+				))}
 			</div>
 			<header className="flex f-arnd f-align-c">
 				<div><span onClick={openNav}>O</span></div>
