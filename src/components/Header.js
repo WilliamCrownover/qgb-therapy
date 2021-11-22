01f5b978565aa0d6a61d259988fffd32143be980
William Crownover <williamcrownover1@gmail.com>
2021-11-21 16:23:42 -0800

    Created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+
+	return (
+		<header className="flex f-arnd f-align-c">
+			<h1>QGB Therapy</h1>
+			<div>
+				<h2>Psychotherapy services by Dr. Quintin Bailey, PsyD</h2>
+				<h2>Supervised by Dr. Krista Jordan, PhD, ABPP</h2>
+			</div>
+		</header>
+	);
+}
