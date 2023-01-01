c0ac25747f59337b8a6090236aa8c65039baed4d
William Crownover <williamcrownover1@gmail.com>
2023-01-01 14:07:02 -0800

    Update covid page info

--- src/pages/Covid.js
+++ src/pages/Covid.js
@@ -11,10 +11,10 @@ export default function Covid() {
 					<h4>The following precautions are being taken for in-office sessions</h4>
 					<ul>
 						<li>Patients experiencing symptoms of COVID-19, patients who have tested positive for COVID-19 in the past two weeks, and patients that have been in contact with another person who has tested positive for COVID-19 will be given the option to do telephone sessions until it is safe to resume in-person services.</li>
-						<li>Masks are required in the communal spaces (e.g., lobby and hallways). Prior to our first meeting, we will discuss your risk level, personal comfort, and other safety concerns before determining mask procedures during session.</li>
+						<li>Prior to our first meeting, we will discuss your risk level, personal comfort, and other safety concerns before determining mask procedures during session.</li>
 						<li>All surfaces are disinfected between patients.</li>
 						<li>Hand sanitizer is available in the office space.</li>
-						<li>As of December 2021, I am fully vaccinated for COVID-19 including a booster and encourage all who can access vaccinations to do so.</li>
+						<li>As of December 2022, I am fully vaccinated for COVID-19 including a booster and encourage all who can access vaccinations to do so.</li>
 					</ul>
 				</main>
 
