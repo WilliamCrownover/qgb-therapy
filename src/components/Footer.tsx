16b7f91bbfbda07659c9cc5d38ffbb2ac4e9d2d3
William Crownover <williamcrownover1@gmail.com>
2023-03-18 17:43:27 -0700

    Added 'Top Doctor' badge to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,27 +4,47 @@ export const Footer = () => {
 		<footer className='flex f-btw f-align-c'>
 			<p className='copyright'>© 2021</p>
 
-			{/* Professional verification provided by Psychology Today */}
-			<a
-				href='https://www.psychologytoday.com/profile/906011'
-				className='sx-verified-seal'
-				target='_blank'
-				rel='noopener noreferrer'
-			>
-				<img
-					className='verified-pic'
-					src={`${process.env.PUBLIC_URL}/assets/pt.png`}
-					alt='Verified Psychology Today'
+			<div className='flex f-align-c'>
+				{/* Professional verification provided by Psychology Today */}
+				<a
+					href='https://www.psychologytoday.com/profile/906011'
+					className='sx-verified-seal'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						className='verified-pic'
+						src={`${process.env.PUBLIC_URL}/assets/pt.png`}
+						alt='Verified Psychology Today'
+					/>
+				</a>
+				<script
+					type='text/javascript'
+					src='https://member.psychologytoday.com/verified-seal.js'
+					data-badge='14'
+					data-id='906011'
+					data-code='aHR0cHM6Ly93d3cucHN5Y2hvbG9neXRvZGF5LmNvbS9hcGkvdmVyaWZpZWQtc2VhbC9zZWFscy9bQkFER0VdL3Byb2ZpbGUvW1BST0ZJTEVfSURdP2NhbGxiYWNrPXN4Y2FsbGJhY2s='
 				/>
-			</a>
-			<script
-				type='text/javascript'
-				src='https://member.psychologytoday.com/verified-seal.js'
-				data-badge='14'
-				data-id='906011'
-				data-code='aHR0cHM6Ly93d3cucHN5Y2hvbG9neXRvZGF5LmNvbS9hcGkvdmVyaWZpZWQtc2VhbC9zZWFscy9bQkFER0VdL3Byb2ZpbGUvW1BST0ZJTEVfSURdP2NhbGxiYWNrPXN4Y2FsbGJhY2s='
-			/>
-			{/* End Verification */}
+
+				{/* Top Doctor Badge */}
+				<div className="fatd-badge-outer">
+					<div className="fatd-badge-inner">
+						<a
+							className="fatd-badge-link"
+							href="https://www.findatopdoc.com/doctor/85018170-Quintin-Bailey-Psychologist"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="fatd-badge-img"
+								src="https://www.findatopdoc.com/doctor_images/fatd-badge.png"
+								alt="Findatopdoc Top Doctor Badge"
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
+
 		</footer>
 	);
 }
