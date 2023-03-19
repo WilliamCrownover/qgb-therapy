64ec427b702057f9820a0f72ca63f2e5047fd927
William Crownover <williamcrownover1@gmail.com>
2023-03-18 17:42:20 -0700

    Removed quote from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,6 @@ export const Home = () => {
 		<main>
 			<div className='container-img-quote'>
 				<div className='hero-img' style={{ backgroundImage: 'url(' + homeImage + ')' }}></div>
-				<h3 className='quote'>By shifting the ways in which we make meaning in the world around us, we shift our experience of the world.</h3>
 			</div>
 		</main>
 	);
