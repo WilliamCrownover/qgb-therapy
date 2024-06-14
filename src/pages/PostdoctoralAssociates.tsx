3f2a7404566e655c6926b323a637bfcb0b2b03c3
William Crownover <williamcrownover1@gmail.com>
2024-06-13 20:24:31 -0700

    Created PostdoctoralTraining and PostdoctoralAssociates pages

---
+++ src/pages/PostdoctoralAssociates.tsx
@@ -0,0 +1,10 @@
+import { PageContainer } from '../components/PageContainer';
+
+export const PostdoctoralAssociates = () => {
+
+	return (
+		<PageContainer pageTitle='Postdoctoral Associates'>
+			<></>
+		</PageContainer>
+	);
+}
---
+++ src/pages/PostdoctoralTraining.tsx
@@ -0,0 +1,99 @@
+import { PageContainer } from '../components/PageContainer';
+import { TabbedParagraph } from '../components/TabbedParagraph';
+
+export const PostdoctoralTraining = () => {
+
+	return (
+		<PageContainer pageTitle='Postdoctoral Training'>
+			<h4>Position Description</h4>
+			<TabbedParagraph content={'QGB Therapy LLC offers a 12-24-month postdoctoral program during which postdoctoral associates will be supported by Dr. Bailey in cultivating essential skills for private practice through supervision and consultation. This postdoctoral program is intentionally designed to deepen understanding of psychoanalytic theory and support integrating theory with culturally humble clinical services. There are three areas of emphasis available (postdoctoral associates may choose more than one area of emphasis):'}/>
+			<br />
+			<ul>
+				<li>culturally humble care for personality disorders</li>
+				<li>culturally humble care for severe dissociative disorders</li>
+				<li>culturally humble care for trans and gender-expansive folks</li>
+			</ul>
+			<br />
+			<TabbedParagraph content={'This postdoctoral program is flexible and customizable for each postdoctoral associate, encouraging and supporting a sustainable practice for each clinician. Each clinician decides their hours, caseload size, and practice structure (electronic health record, fee/sliding-scale, advertising and referrals, etc.) with guidance from Dr. Bailey. This program offers a competitive fee-split and allows postdoctoral associates to take their full caseload with them once licensed.'}/>
+			<br />
+			<TabbedParagraph content={'While this postdoctoral program is structured to allow for as little overhead as possible, it is recommended to have an additional source of income during the first few months of the program while building up a caseload. Earnings vary depending on patient and clinician availability. Dr. Bailey actively maintains several referral sources for the postdoctoral program as well as mentoring postdoctoral associates in establishing their own referrals networks.'}/>
+			<br />
+			<TabbedParagraph content={'Patients are from throughout California from various diverse backgrounds and experiences. All patients are fee for service, and postdoctoral associates are supported in setting their own fee and deciding what percentage of their caseload is sliding scale.'}/>
+			<br />
+			<TabbedParagraph content={'In addition, Dr. Bailey provides active support throughout the licensure process to encourage postdoctoral associate success. This includes support with the Examination for Professional Practice in Psychology (EPPP), the California Laws and Ethics Exam (CPLEE), and other licensing requirements. Postdoctoral associates have access to Dr. Bailey via phone and email 24/7 and are required to contact them should there be any crisis situations. Postdoctoral associates are required to register as psychological associates and be in good standing with the California Board of Psychology.'}/>
+			<br />
+			<TabbedParagraph content={'For those interested in learning more about this postdoctoral program, please email Dr. Bailey a copy of your current CV and a cover letter. Positions are available year-round, space permitting. The program is currently open to those seeking a postdoctoral position in California.'}/>
+			<br />
+			<TabbedParagraph content={'Postdoctoral associates are selected based on a comprehensive review of education, clinical experience, and interpersonal “fit” with QGB Therapy LLC. Applicants are selected based on CV, letter of interest, interview with Dr. Bailey, and references. Decisions are made irrespective of age, gender, race, religion, culture, national affiliation, ability status, language, socioeconomic status, sexual orientation, gender identity, or any other identities.'}/>
+
+			<h4>Formal Training Hours</h4>
+			<ul>
+				<li>Individual supervision – 1-hour per week</li>
+				<li>Group supervision – 2-hours per week</li>
+				<li>Independent study or additional consultation – 1-hour per week</li>
+			</ul>
+			<br />
+			<TabbedParagraph content={'Above are the only routinely scheduled training hours for postdoctoral associates. Additional training, supervision, or consultation may be scheduled as requested by either the postdoctoral associate or Dr. Bailey. Supervision is scheduled based on postdoctoral fellow and Dr. Bailey’s availability.'}/>
+
+			<h4>Training Objectives</h4>
+			<ul>
+				<li>Integration of research and best practice in psychoanalytic psychology</li>
+				<li>Development and maintenance of report and boundaries, especially with complex presentations</li>
+				<li>Psychoanalytic and DSM-V diagnostics, including differential diagnoses</li>
+				<li>Making appropriate referrals when ruling out medical conditions or physiological causes</li>
+				<li>Identifying and attending to culturally humble care and interventions</li>
+				<li>Managing crisis with patients and making referrals to higher levels of care when appropriate</li>
+				<li>Strong foundation in psychoanalytic therapy, including working with transference, countertransference, enactments, projections, projective identification, dream interpretation, etc.</li>
+				<li>Use of appropriate self-disclosure; understanding of clinician as part of the treatment frame (therapeutic use of self)</li>
+				<li>Use of somatic information to inform treatments</li>
+				<li>Management of high-risk terminations</li>
+				<li>Learning to set and maintain appropriate boundaries for burn-out prevention</li>
+				<li>Establishing boundaries of professional competency/expertise</li>
+				<li>Establishing comfort in seeking additional consultation and/or supervision as needed</li>
+				<li>Comprehensive understanding of professional conduct, laws, ethics, and other related standards for the practice of Psychology in California</li>
+				<li>How to build and maintain effective referral relationships with fellow mental health providers (psychologists, psychiatrists, social workers, etc.)</li>
+				<li>How to build and maintain effective referral relationships with other professionals (medical physicians, occupational therapists, religious or spiritual leaders, etc.)</li>
+				<li>How to identify appropriate referrals and resources for patients in addition to individual outpatient treatment</li>
+				<li>How to effectively market a fee-for-service private practice</li>
+				<li>Private practice management skills including but not limited to, electronic health record management, liability insurance, business status (i.e., S-Corp, LLC), accounting and taxes, etc.</li>
+			</ul>
+
+			<h4>Required Readings</h4>
+			<h5>All Postdoctoral Associates</h5>
+			<ul>
+				<li><i>Essential Psychodynamic Psychotherapy: An Acquired Art</i> by Teri Quatman</li>
+				<li><i>Sabina Spielrein: The Woman and the Myth</i> by Angela Sells</li>
+				<li><i>Conflict is Not Abuse</i> by Sarah Schulman</li>
+			</ul>
+			<h5>Personality Disorder Emphasis</h5>
+			<ul>
+				<li><i>Envy and Gratitude and Other Works, 1946 – 196</i> by Melanie Klein</li>
+				<li><i>Selected Melanie Klein</i> edited by Juliet Mitchell</li>
+			</ul>
+			<h5>Dissociation Emphasis</h5>
+			<ul>
+				<li><i>Understanding and Treating Dissociative Identity Disorder</i> by Elizabeth Howell</li>
+				<li><i>Intensive Psychotherapy for Persistent Dissociative Processes: The Fear of Feeling Real</i> by Richard Chefetz</li>
+			</ul>
+			<h5>Trans and Gender-Expansive Emphasis</h5>
+			<ul>
+				<li><i>In a Queer Time and Place: Transgender Bodies, Subcultural Lives</i> by J. Jack Halberstam</li>
+				<li><i>Transgender History</i> by Susan Stryker</li>
+			</ul>
+			<br />
+			<TabbedParagraph content={'This list represents the foundational texts included in the training. Additional texts and materials may be provided, and postdoctoral associates are encouraged to engage in their own continued education through certificates, courses, texts, conferences, etc.'}/>
+
+			<h4>Postdoctoral Associate Qualifications</h4>
+			<ul>
+				<li>A doctoral degree in Clinical Psychology (PhD or PsyD) from an APA accredited institution</li>
+				<li>Completion of 1,500 pre-doctoral hours of supervised professional experience at an APA accredited pre-doctoral internship</li>
+				<li>Actively working towards licensure as a Clinical Psychologist in California</li>
+				<li>Strong understanding of the APA ethics code, laws, and legal mandates that apply to mental health professionals</li>
+				<li>Ability to remain calm under pressure</li>
+				<li>Ability to function independently with appropriate supervision</li>
+				<li>Excellent attendance, punctuality, collegial and diplomatic behavior, and work performance</li>
+			</ul>
+
+		</PageContainer>
+	);
+}
